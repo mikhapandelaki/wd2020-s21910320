5c9a69c1815f3b74742b7ae23e42a40aa9b7cb80
mikhapandelaki <70097538+mikhapandelaki@users.noreply.github.com>
2020-11-09 23:13:41 +0800

    push pertemuan 23- javascript&bmi

---
+++ pertemuan_23/pertemuan_23/index.js
@@ -0,0 +1,6 @@
+let umur = 18;
+let namaDepan = "Mikha";
+let namaBelakang = "Pandelaki";
+console.log(
+    "Nama Saya" + namaDepan + " " + namaBelakang + ". umur saya adalah " + umur + "tahun"
+);
